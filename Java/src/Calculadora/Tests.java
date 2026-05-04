c10d33d60fa7f1e3e5665391faa78775e1f817a9
Javi <itsjavii1610@gmail.com>
2026-05-04 11:10:50 +0200

    Calculadora

--- Java/src/Calculadora/Tests.java
+++ Java/src/Calculadora/Tests.java
@@ -1,4 +1,54 @@
-package Calculadora;
+    package Calculadora;
 
-public class Tests {
-}
+    import org.junit.jupiter.api.Test;
+    import static org.junit.jupiter.api.Assertions.*;
+
+    public class Tests {
+
+        @Test
+        void testSumar() {
+            Calculadora calc = new Calculadora();
+            assertEquals(5, calc.sumar(2, 3));
+        }
+
+        @Test
+        void testSumarNegativos() {
+            Calculadora calc = new Calculadora();
+            assertEquals(-5, calc.sumar(-2, -3));
+        }
+        @Test
+        void testRestar() {
+            Calculadora calc = new Calculadora();
+            assertEquals(2, calc.restar(5, 3));
+        }
+
+        @Test
+        void testMultiplicar() {
+            Calculadora calc = new Calculadora();
+            assertEquals(20, calc.multiplicar(5, 4));
+        }
+
+        @Test
+        void testMultiplicarCero() {
+            Calculadora calc = new Calculadora();
+            assertEquals(0, calc.multiplicar(5, 0));
+        }
+
+        @Test
+        void testDivision() {
+            Calculadora calc = new Calculadora();
+            assertEquals(5, calc.dividir(10, 2));
+        }
+
+        @Test
+        void testDivisionCero() {
+            Calculadora calc = new Calculadora();
+            assertThrows(ArithmeticException.class, () -> calc.dividir(10, 0));
+        }
+
+        @Test
+        void testDivisionNegativos() {
+            Calculadora calc = new Calculadora();
+            assertEquals(1, calc.dividir(-5, -5));
+        }
+    }
